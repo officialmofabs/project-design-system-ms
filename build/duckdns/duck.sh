49162a8f01ca7b858d2f7cc491ebf2fa12d95f40
Projectoperations <support@igedevteam.onnmicrosoft.com>
2024-11-08 20:37:41 +0000

    maoin

---
+++ build/duckdns/duck.sh
@@ -0,0 +1,3 @@
+DOMAIN=projectoperations
+TOKEN=a1411e13-5bc9-4842-b437-722f8908c20d
+echo url="https://www.duckdns.org/update?domains=projectoperations&token=a1411e13-5bc9-4842-b437-722f8908c20d&ip=" | curl -k -o ~/duckdns/duck.log -K -
